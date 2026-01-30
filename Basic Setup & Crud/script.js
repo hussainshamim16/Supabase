e41286790d0a0eb9ff88894f47fe02ae5d9c7a26
HUSSAIN SHAMIM <hj422748@gmail.com>
2026-01-30 11:34:17 +0500

    connection_succesfully_complete

--- Basic Setup & Crud/script.js
+++ Basic Setup & Crud/script.js
@@ -1,26 +1,37 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-document.getElementById("studentForm").addEventListener("submit", function (e) {
-    e.preventDefault()
+const SUPABASE_URL = "https://nbzlxxpoaoozjmlthbgm.supabase.co";
+let ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iemx4eHBvYW9vemptbHRoYmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MDI1NzMsImV4cCI6MjA3ODk3ODU3M30.asMOEZ9Sa5RAum0DMQ1XasN8c8lzXBpqZKEwF2NW9aA";
 
-    const resData = new FormData(this)
-    const studentObject = {
-        ST_NAME: resData.get("name"),
-        ST_EMAIL: resData.get("email"),
-        ST_AGE: resData.get("age"),
-        ST_CLASS: resData.get("class"),
-        ST_DOB: resData.get("dob"),
+
+const supabase = createClient(SUPABASE_URL, ANON_KEY);
+
+async function getUsers() {
+    const { data, error } = await supabase
+        .from("Student Data")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(data);
     }
+}
 
-
-    console.log("submit", studentObject)
-})
-
-console.log(supabase.createClient)
+getUsers();
 
 
 
-let project_id = "nbzlxxpoaoozjmlthbgm"
-let SUPABASE_URL = "https://nbzlxxpoaoozjmlthbgm.supabase.co"
-let Anon_Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iemx4eHBvYW9vemptbHRoYmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MDI1NzMsImV4cCI6MjA3ODk3ODU3M30.asMOEZ9Sa5RAum0DMQ1XasN8c8lzXBpqZKEwF2NW9aA";
-// create supabAse connection
-const supabase = supabase.createClient(SUPABASE_URL, Anon_Key)
+// document.getElementById("studentForm").addEventListener("submit", function (e) {
+//     e.preventDefault()
+
+//     const resData = new FormData(this)
+//     const studentObject = {
+//         ST_NAME: resData.get("name"),
+//         ST_EMAIL: resData.get("email"),
+//         ST_AGE: resData.get("age"),
+//         ST_CLASS: resData.get("class"),
+//         ST_DOB: resData.get("dob"),
+//     }
+//     console.log("submit", studentObject)
+// })
